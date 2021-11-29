1cc1eddcd3af6478620e0dc5cacda7a05541d235
Marijan <mjuresic@gmail.com>
2021-11-29 23:40:12 +0100

    A Domino Piling problem

---
+++ A_Domino_piling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class A_Domino_piling {
+     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int k=n*m/2;
+        System.out.println(k);       
+    }
+    
+}
