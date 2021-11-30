141b563ec8432941d53b8bf8dd41fe87b37c0cc7
Marijan <mjuresic@gmail.com>
2021-12-01 00:27:45 +0100

    A Beautiful Matrix problem

---
+++ A_Beautiful_Matrix.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class A_Beautiful_Matrix
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// Input
+		Scanner sc = new Scanner(System.in);
+		int i = 1;
+		int j = 1;
+		for (i = 1; i <= 5; i++) {
+			if (sc.nextInt() == 1) {
+				break;
+			}
+
+			// Increment;
+			if (i == 5) {
+				j++;
+				i = 0;
+			}
+		}
+
+		System.out.println(Math.abs(i-3)+Math.abs(j-3));
+	}	
+}
