f5e046ab44c9a1d5ad8439d829af824cdeb20c99
Marijan <mjuresic@gmail.com>
2021-12-02 05:00:39 +0100

    try with the resource for scanner leak warning

--- A_Beautiful_Matrix.java
+++ A_Beautiful_Matrix.java
@@ -4,22 +4,22 @@ public class A_Beautiful_Matrix
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// Input
-		Scanner sc = new Scanner(System.in);
-		int i = 1;
-		int j = 1;
-		for (i = 1; i <= 5; i++) {
-			if (sc.nextInt() == 1) {
-				break;
-			}
+		try (// Input
+		Scanner sc = new Scanner(System.in)) {
+			int i = 1;
+			int j = 1;
+			for (i = 1; i <= 5; i++) {
+				if (sc.nextInt() == 1) {
+					break;
+				}
 
-			// Increment;
-			if (i == 5) {
-				j++;
-				i = 0;
+				// Increment;
+				if (i == 5) {
+					j++;
+					i = 0;
+				}
 			}
+			System.out.println(Math.abs(i-3)+Math.abs(j-3));
 		}
-		sc.close();
-		System.out.println(Math.abs(i-3)+Math.abs(j-3));
 	}	
 }
