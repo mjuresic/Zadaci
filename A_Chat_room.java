1fe83a5a8cc0e02c99d75433d8a9e1a9dea09ad2
Marijan <mjuresic@gmail.com>
2021-12-16 23:33:02 +0100

    A Chat Room problem

---
+++ A_Chat_room.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class A_Chat_room
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// Input
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+
+		Character[] hello = {'h', 'e', 'l', 'l', 'o'};
+		int index = 0;
+		char lookingFor = hello[index];
+		for (char c : s.toCharArray()) {
+			if (c == lookingFor && index == 4) {
+				System.out.println("YES");
+				return;
+			} else if (c == lookingFor) {
+				index++;
+				lookingFor = hello[index];
+			}
+		}
+
+		System.out.println("NO");
+	}	
+}
