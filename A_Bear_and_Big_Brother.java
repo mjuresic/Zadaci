1ca6ab0d36d5f86df143e63d3624037d4d748c85
Marijan <mjuresic@gmail.com>
2021-12-24 15:31:04 +0100

    resolved errors ad Bear and big brother problem

--- A_Bear_and_Big_Brother.java
+++ A_Bear_and_Big_Brother.java
@@ -2,14 +2,11 @@ import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.io.PrintStream;
 import java.util.StringTokenizer;
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.InputStream;
 
-public class Main {
+public class A_Bear_and_Big_Brother {
     public static void main(String[] args) {
         InputStream inputStream = System.in;
         OutputStream outputStream = System.out;
