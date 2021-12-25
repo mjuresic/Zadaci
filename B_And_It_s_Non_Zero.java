f5706f18f9b164ad11b5bf6d6b5869530c9805b8
Marijan <mjuresic@gmail.com>
2021-12-25 23:52:15 +0100

    George and accommodation problem

--- B_And_It_s_Non_Zero.java
+++ B_And_It_s_Non_Zero.java
@@ -12,18 +12,23 @@ public class B_And_It_s_Non_Zero
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// Input
+        int a = 0;
+        int b = 200000;
+        Integer[] polje = Collections.nCopies(20, b-a+1).toArray(new Integer[0]);
+        for(int num=a;num<=b;num++){
+            int num2=num;
+            for (int k = 0; num2 != 0; k++, num2 >>>= 1) {
+                if ((num2 & 1) != 0) polje[k]=polje[k]-1;
+              }
+        }
+
 		Scanner sc = new Scanner(System.in);
 		int t=sc.nextInt();
         for(int i=0;i<t;i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            Integer[] polje = Collections.nCopies(20, b-a+1).toArray(new Integer[0]);
-            for(int num=a;num<=b;num++){
-                int num2=num;
-                for (int k = 0; num2 != 0; k++, num2 >>>= 1) {
-                    if ((num2 & 1) != 0) polje[k]=polje[k]-1;
-                  }
-            }
+
+            a = sc.nextInt();
+            b = sc.nextInt();
+
             //for(int m=0;m<polje.length;m++){
             //    System.out.print(b-a+1-polje[m]);
            // }
