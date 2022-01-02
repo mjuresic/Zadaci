4a055b8179628f0df47e001c7a8906f527fe5a94
Marijan <mjuresic@gmail.com>
2022-01-02 23:37:59 +0100

    Even odds problem

---
+++ A_Even_Odds.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class A_Even_Odds
+{
+   public static void main(String args[])
+   {
+	Scanner z=new Scanner(System.in);
+	long n=z.nextLong();
+	long k=z.nextLong();
+	if(n%2==0)
+	{
+	   if(k==n/2)
+		System.out.println(n-1);
+	   if(k>n/2)
+		System.out.println(2*(k-n/2));
+	   if(k<n/2)
+		System.out.println((2*k)-1);
+	}
+	else
+	{
+	   if(k==n/2+1)
+		System.out.println(n);
+	   if(k>n/2+1)
+		System.out.println(2*(k-(n/2+1)));
+	   if(k<n/2+1)
+		System.out.println((2*k)-1);
+	}
+   }
+}
