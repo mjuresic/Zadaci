a66e07f7bc90bb665b8097a0c5a01c91eeb798f0
Marijan <mjuresic@gmail.com>
2022-01-04 23:49:56 +0100

    A Calculating Function problem

---
+++ A_Calculating_Function.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class A_Calculating_Function
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// Input
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		long fn;
+		
+		// Calculate
+		if (n%2 == 0) fn = n/2;
+		else fn = (-1)*(n/2 + 1);
+		
+		// Output
+		System.out.println(fn);
+	}	
+}
