08538cd24c89ec57632b40ee33678ac1584f8ec2
Marijan <mjuresic@gmail.com>
2022-01-11 23:53:21 +0100

    A Anton and Letters problem

---
+++ A_Anton_and_Letters.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.HashSet;
+public class A_Anton_and_Letters {
+    public static void main(String[] args) {
+
+        Scanner myscanner = new Scanner(System.in);
+       HashSet<Character> test = new HashSet<>();
+		
+		String given = myscanner.nextLine();
+		
+		for(int i=0;i<given.length();i++){
+			if(given.charAt(i)>= 'a' && given.charAt(i)<= 'z')
+				test.add(given.charAt(i));
+		}
+		System.out.print(test.size());
+    }
+}
