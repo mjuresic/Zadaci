8cd03ed18ec98966d6c447bdee01550da4186203
Marijan <mjuresic@gmail.com>
2022-01-25 23:50:15 +0100

    A Kefa and First Steps

---
+++ A_Kefa_and_First_Steps.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+/**
+ *
+ * @author Yehia Ezzat
+ */
+public class A_Kefa_and_First_Steps {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int[] array = new int[size];
+        int current=1,max=1,i=0;
+        for(i=0;i<size;i++)
+            array[i]=scan.nextInt();
+        for(i=1;i<array.length;i++){
+            if(array[i]>=array[i-1])
+                current++;
+            else
+                current=1;
+            max= Math.max(current,max);
+        }
+        System.out.println(max);
+    }
+}
